e9c6086c9c4a669742d62307baffdbe65cb68ab0
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-13 08:03:16 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
 // Code DigitalClicker Component Here
+import React,{Component} from 'react';
+
+
+export default class DigitalClicker extends Component{
+  
+  constructor(){
+    super();
+    this.state={
+      timesClicked:0,
+    }
+  }
+  
+  handleClick=()=> {
+    this.setState( prev =>
+    ({timesClicked:prev.timesClicked+1})
+  )}
+  
+  render(){
+    return (
+      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+      )
+  }
+}

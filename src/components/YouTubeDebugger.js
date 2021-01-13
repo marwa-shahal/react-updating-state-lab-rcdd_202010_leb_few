173109da45aa1e35631c2e0389a0afde201f4da3
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-13 08:05:27 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,24 @@
 // Code YouTubeDebugger Component Here
+import React,{Component} from 'react';
+
+
+export default class YouTubeDebugger extends Component{
+  
+  constructor(){
+    super();
+    this.state={
+      timesClicked:0,
+    }
+  }
+  
+  handleClick=()=> {
+    this.setState( prev =>
+    ({timesClicked:prev.timesClicked+1})
+  )}
+  
+  render(){
+    return (
+      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+      )
+  }
+}

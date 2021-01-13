f2ee69e72afcea88b0257e32ceaf7d7daa317623
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-13 08:07:41 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -7,18 +7,25 @@ export default class YouTubeDebugger extends Component{
   constructor(){
     super();
     this.state={
-      timesClicked:0,
-    }
+        errors: [],
+        user: null,
+        settings: {
+        bitrate: 8,
+        video: {
+            resolution: '1080p'
+            }
+        }
+     }
   }
   
-  handleClick=()=> {
+  handleUtube=()=> {
     this.setState( prev =>
     ({timesClicked:prev.timesClicked+1})
   )}
   
   render(){
     return (
-      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+      <button className="bitrate" onClick={this.handleUtube}>{this.state.timesClicked}</button>
       )
   }
 }
